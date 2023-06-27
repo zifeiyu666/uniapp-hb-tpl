1a09cf07c943b91d17a9a62c02470d8fe1bb2832
yuxp <1039354317@qq.com>
2023-06-27 17:28:39 +0800

    feat:init

---
+++ App.vue
@@ -0,0 +1,24 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style lang="scss">
+	// uview 变量
+	@import "@/uni_modules/uview-ui/index.scss";
+
+	// 每个页面公共css
+	body,
+	uni-page-body {
+		background-color: #999 !important;
+	}
+</style>
---
+++ main.js
@@ -0,0 +1,32 @@
+import App from './App'
+
+
+// uview 引入
+import uView from '@/uni_modules/uview-ui'
+Vue.use(uView)
+
+// #ifndef VUE3
+import Vue from 'vue'
+import './uni.promisify.adaptor'
+Vue.config.productionTip = false
+App.mpType = 'app'
+const app = new Vue({
+	...App
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import {
+	createSSRApp
+} from 'vue'
+export function createApp() {
+	const app = createSSRApp(App)
+	return {
+		app
+	}
+}
+// #endif
+
+// 引入请求封装，将app参数传递到配置中
+require('config/request.js')(app)
